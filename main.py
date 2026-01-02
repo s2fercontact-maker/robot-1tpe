353fb28bca8a76b97073f5ca6304245acdf22d81
s2fercontact-maker <s2fercontact@gmail.com>
2026-01-02 03:56:21 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,19 @@
-import time
+from http.server import BaseHTTPRequestHandler, HTTPServer
+import threading
+
+# Petit serveur pour garder Render content (Gratuit)
+class HealthCheckHandler(BaseHTTPRequestHandler):
+    def do_GET(self):
+        self.send_response(200)
+        self.end_headers()
+        self.wfile.write(b"Robot en ligne")
+
+def run_health_check():
+    server = HTTPServer(('0.0.0.0', 10000), HealthCheckHandler)
+    server.serve_forever()
+
+# Lance le serveur dans un fil séparé
+threading.Thread(target=run_health_check, daemon=True).start() import time
 
 # TES INFOS
 ID_1TPE = "zackbizzza"
